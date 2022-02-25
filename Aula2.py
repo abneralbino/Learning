80a208ac9ccd85f6f9689ffb3eaef35b0b85d65f
abneralbino <99372984+abneralbino@users.noreply.github.com>
2022-02-24 22:24:34 -0300

    Add files via upload
    Leraning "Conditinals"

---
+++ Aula2.py
@@ -0,0 +1,11 @@
+#Condicionais
+#if, elif, else
+'''
+Let's go out tonight?
+If I finish my work, I can go. 
+'''
+work_is_finished = False
+if work_is_finished == True:
+  print("Yes, let's go out")
+else:
+  print("Sorry, I can't go")
